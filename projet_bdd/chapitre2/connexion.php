50e697cbf7a579a77621431bf101753ecb1ecfd5
khadijaceurrouj57-coder <khadijaceurrouj57@gmail.com>
2026-03-11 12:03:44 +0000

    php

--- projet_bdd/chapitre2/connexion.php
+++ projet_bdd/chapitre2/connexion.php
@@ -5,7 +5,6 @@ $username = 'root';
 $password = '';
 
 try {
-  //ouvrir connection
   $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
   $pdo->setAttribute(  PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    
