4dd12e048d3ae15614d0ccd3b980e408789ef909
khadijaceurrouj57-coder <khadijaceurrouj57@gmail.com>
2026-03-26 09:49:10 +0000

    php

--- projet_poo/chapitre7/article.php
+++ projet_poo/chapitre7/article.php
@@ -1,5 +1,5 @@
 <?php
-class article{
+class Article{
     public $titre;
     public $contenu;
 
@@ -8,6 +8,13 @@ public function afficher(){
 
 }
 }
-$article1 = new Article();
-$article1->titre ="introduction a php";
 
+$article1 = new Article();
+$article1->titre = "introduction a php <br>";
+$article1->contenu = "PHP est un langage de script côté serveur <br>";
+echo $article1->afficher();
+
+$article2 = new Article();
+$article2->titre = "Programmation orientée objet <br>";
+$article2->contenu = "La POO facilite la modularité et la maintenance <br>";
+echo "<br>" . $article2->afficher();
